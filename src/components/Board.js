d594c7896adae22456bfe75c36f95f80edacd3e3
tsfromm <48682631+tsfromm@users.noreply.github.com>
2019-04-23 13:17:30 -0400

    Add files via upload

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,63 +1,72 @@
-/* eslint-disable no-useless-constructor */
-import React, { Component } from "react";
-import "../css/Board.css";
-import Note from "./Note";
+import React, {Component} from 'react';
+import '../css/Board.css';
+import Note from './Note';
 
 class Board extends Component {
-  constructor() {
-    super();
-    this.state = {
-      notes: []
-    };
-  }
-
-  addNote() {
-    this.state.notes.push({
-      id: Date.now()
-    });
-    this.setState({
-      notes: this.state.notes
-    });
-  }
-
-  deleteNote(id) {
-    let newNoteArr = this.state.notes;
-    newNoteArr.map((note, index) => {
-      if (id === note.id) {
-        newNoteArr.splice(index, 1);
+    constructor() {
+        super();
+   this.state = {
+        notes: [
+            // {title: "Harry Potter and the Sorcerer's Stone",
+            //  body: "JK Rowling, 1997"},
+            // {title: "Harry Potter and the Chamber of Secrets",
+            //  body: "JK Rowling, 1998"},
+            // {title: "Harry Potter and the Prizoner of Azkaban", 
+            //  body: "JK Rowling, 1999"}
+        ]
+   };
+    }//constructor 
+    addNote() {
+        this.state.notes.push({
+          id: Date.now()
+        });
+        this.setState({
+          notes: this.state.notes
+        });
       }
-    });
-    this.setState({
-      notes: newNoteArr
-    });
-  }
+    
+      deleteNote(id) {
+        let newNoteArr = this.state.notes;
+        // eslint-disable-next-line
+        newNoteArr.map((note, index) => {
+          if (id === note.id) {
+            newNoteArr.splice(index, 1);
+          }
+        });
+        this.setState({
+          notes: newNoteArr
+        });
+      }
+    // addShelf() {
+    //   //add function to add new shelf
+    // }
+    render() {
+        return (
+          <div className="all">
+            <div className="div-board">
+              <div className="row">
+                {this.state.notes.map(note => {
+                  return (
+                    <Note
+                      key={note.id}
+                      id={note.id}
+                      deleteHandler={this.deleteNote.bind(this)}
+                    />
+                  );
+                })}
+              </div>
+              <div>
+                <button
+                  className="btn btn-success add-button"
+                  onClick={this.addNote.bind(this)}
+                >
+                  Add Book
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      }
+}//class Board
 
-  render() {
-    return (
-      <div>
-        <div className="div-board">
-          <div className="row">
-            {this.state.notes.map(note => {
-              return (
-                <Note
-                  key={note.id}
-                  id={note.id}
-                  deleteHandler={this.deleteNote.bind(this)}
-                />
-              );
-            })}
-          </div>
-          <div>
-            <button
-              className="btn btn-success add-button"
-              onClick={this.addNote.bind(this)}
-            >
-              Add
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 export default Board;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,83 +1,93 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React, {Component} from 'react';
-import propTypes from 'prop-types';
 import '../css/Note.css';
+import PropTypes from 'prop-types';
+
+const GENERIC_NOTE_TITLE = "Book Title",
+  GENERIC_NOTE_BODY = "Author, Year";
 
 class Note extends Component {
+    // eslint-disable-next-line
     constructor() {
         super();
-    };
-
-
-        componentWillMount() {
+                this.state = {
+                    notes: [
+                    ]
+                };
+             }
+    componentWillMount() {
+        // eslint-disable-next-line
         this.state = {
-            title: this.props.body,
-            body: this.props.body,
-            image: this.props.body,
-            editMode: false
-        }
+          title: GENERIC_NOTE_TITLE,
+          body: GENERIC_NOTE_BODY,
+          editMode: false
+        };
+      }
+    
+    handleEdit() {
+        this.setState({
+            editMode: true
+        });
     }
 
-handlerEdit() {
-    this.setState({
-        editMode: true
-    });
-}
-
-handlerSave() {
-    this.setState({
-        title: this.refs.titleContent.value,
-        body: this.refs.bodyContent.value,
-        editMode: false
-    });
-}
-
-handlerDelete() {
-    this.props.deleteHandler(this.props.id);
-}
-    
-    render() {
-        let titleElement, bodyElement, buttonArea;
-        if (this.state.editMode) {
-            titleElement = <textarea ref="titleContent" className="title-textarea" defaultValue={this.state.title} ></textarea>
-            bodyElement = <textarea ref="bodyContent" className="body-textarea" defaultValue={this.state.body}></textarea>;
-            buttonArea=<div><button className="btn btn-info" onClick={this.handlerSave.bind(this)}>Save</button></div>
-        } else {
-            titleElement = <h5>{this.state.title}</h5>;
-            bodyElement = <p> {this.state.body}</p>
-            buttonArea = 
-            <div>
-                <button className="btn btn-warning" onClick={this.handlerEdit.bind(this)}>Edit</button>
-                <button className="btn btn-danger" onClick={this.handlerDelete.bind(this)}>Delete</button>
-            </div>
-
-        }
-
-        return (
-            <div className="col-sm-4">
-            <div class="card border-dark mb-3">
-                <div className="card card-view">
-                <img class="card-img-top" src="http://static.lulu.com/cmsmedia/create-page/create_page_images_book.jpg" alt="Card image cap"></img></div>
-                 <div className="card-body">
+    handleSave() {
+        this.setState( {
+            title: this.refs.titleContent.value,
+            body: this.refs.bodyContent.value,
+            editMode: false
+        });
+    }
+    handleDelete() {
+        this.props.deleteHandler(this.props.id);
+      }
+render() {
+    let titleElement, bodyElement, buttonArea;
+    if (this.state.editMode) {
+        titleElement = <textarea ref="titleContent" className="title-textarea" defaultValue={this.state.title}></textarea>
+        bodyElement = <textarea ref="bodyContent" className="body-textarea" defaultValue={this.state.body}></textarea>
+        buttonArea = <div><button className = "btn btn-info" onClick={this.handleSave.bind(this)}>Save</button></div>
+    } else {
+        titleElement = <h5>{this.state.title}</h5>;
+        bodyElement = <p>{this.state.body}</p>;
+        buttonArea = (<div><button className="btn btn-warning" onClick = {this.handleEdit.bind(this)}>Edit</button>
+                           <button className="btn btn-danger" onClick = {this.handleDelete.bind(this)}>Delete</button>
+                        <form>
+                            <label>
+                                    Have read:
+                                    <input
+                                        name="haveRead"
+                                        type="checkbox"
+                                        checked={this.state.haveRead}
+                                        onChange={this.handleInputChange} />
+                                    </label>
+                                
+                                </form>
+                         </div>);
+    };
+    return (
+//render html components for the card/note. Board.js deals with the area the card/note appears on.
+        <div className="col-sm-4">
+        <div className="cardBorder">
+            <div className="card card-view">
+            <img class="card-img-top" src="http://static.lulu.com/cmsmedia/create-page/create_page_images_book.jpg" alt="book thumbnail"></img></div>
+                <div className="card-body">
                     {titleElement}
                     {bodyElement}
                     {buttonArea}
+                </div>
             </div>
-            </div>
-
-            </div>
-        );
-    }
+        </div>
+    );
+}
 }
 
-
-Note.defaultProps = {
-    title: "Title",
-    body: "Body",
+Note.defaultProps={
+    title: "Book Title",
+    body: "Author, Year"
 };
 
-Note.propTypes = {
-    title: propTypes.string
-}
+Note.propTypes={
+    title: PropTypes.string
+
+};
 
 export default Note;
